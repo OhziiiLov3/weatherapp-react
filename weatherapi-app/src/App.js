1175c9037c1c3e8f1ec0027fa2c74ce131b82928
ohziiilov3 <klbaskerville0520@gmail.com>
2021-11-04 09:18:31 -0700

    added fetch, get response not working yet

--- weatherapi-app/src/App.js
+++ weatherapi-app/src/App.js
@@ -1,14 +1,34 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import './App.css'
 
 const App = () => {
 
-  const apiKey = '233d7e0ca24b8004061ee0c5dab4fa94';
+  const apiKey = '233d7e0ca24b8004061ee0c5dab4fa94'
   const [weatherData, setWeatherData] = useState([{}])
+  const [city, setCity] = useState("")
+
+  const getWeather = (event) => {
+    if(event.key === "Enter") {
+      fetch('https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&APPID=${apiKey}').then(
+        response => response.json()
+      ).then(
+        data => {
+          setWeatherData(data)
+        }
+      )
+    }
+  }
 
   return (
     <div className="container">
-      <input className="input" placeholder="Enter City.."/>
+      <input 
+      className="input" 
+      placeholder="Enter City.."
+      onChange={ e => setCity(e.target.value)}
+      value = {city}
+      onKeyPress = {getWeather}
+
+      />
     </div>
   )
 }
